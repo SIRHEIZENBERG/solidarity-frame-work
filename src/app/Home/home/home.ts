127b9b1050799ec054d121317bc39bb295cdc6a9
SIRHEIZENBERG <xmalcolm66@gmail.com>
2025-10-20 16:54:56 +0300

    Initial commit for Solidarity Framework

--- src/app/Home/home/home.ts
+++ src/app/Home/home/home.ts
@@ -5,10 +5,11 @@ import { Sessions } from '../../components/sessions/sessions';
 import { SessionTimes } from '../../components/session-times/session-times';
 import { Faqs } from '../../components/faqs/faqs';
 import { Testimonials } from '../../components/testimonials/testimonials';
+import { OurTeam } from '../../our-team/our-team';
 
 @Component({
   selector: 'app-home',
-  imports: [LandingBanner, AboutArea, Sessions, SessionTimes, Faqs, Testimonials],
+  imports: [LandingBanner, AboutArea, Sessions, SessionTimes, Faqs, Testimonials, OurTeam],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
